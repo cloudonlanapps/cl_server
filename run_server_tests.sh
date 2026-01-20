b54c5e5e64e5c5e62e23e7da539383b3969127b6
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2026-01-20 12:53:32 +0530

    run all server tests from workspace

---
+++ run_server_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set test artifact directory (optional - will use default if not set)
+export TEST_ARTIFACT_DIR=/tmp/cl_server_test_artifacts
+
+# Clean up any previous test artifacts
+rm -rf $TEST_ARTIFACT_DIR
+
+
+pushd services/packages/cl_ml_tools ; uv run pytest tests ; popd
+pushd services/auth ; uv run pytest tests ; popd
+pushd services/compute ; uv run pytest tests ; popd
+pushd services/store ; uv run pytest tests ; popd
+
+
