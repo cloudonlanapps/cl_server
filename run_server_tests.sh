f4491008ca1c02424e5438143cdf42db4dc1258d
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2026-02-03 11:39:13 +0530

    run all tests

--- run_server_tests.sh
+++ run_server_tests.sh
@@ -6,9 +6,10 @@ export TEST_ARTIFACT_DIR=/tmp/cl_server_test_artifacts
 # Clean up any previous test artifacts
 rm -rf $TEST_ARTIFACT_DIR
 
-
-pushd services/packages/cl_ml_tools ; uv run pytest tests ; popd
-pushd services/auth ; uv run pytest tests ; popd
-pushd services/compute ; uv run pytest tests ; popd
+uv run pytest services/auth/tests
+uv run pytest services/packages/cl_ml_tools/tests
+uv run pytest services/compute/tests
+uv run pytest services/store/tests --auth-url=http://localhost:8010 --compute-url=http://localhost:8012 --username=admin --password=admin 
+uv run pytest sdks/pysdk/tests/ --auth-url=http://localhost:8010 --compute-url=http://localhost:8012 --store-url=http://localhost:8011 --username=admin --password=admin
 
 
