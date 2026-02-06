05044dfb95510e2227f046e91df1934e179cbbb9
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2026-02-06 11:47:31 +0530

    setup correct cache folder for uv

--- run_server_tests.sh
+++ run_server_tests.sh
@@ -2,6 +2,7 @@
 
 # Set test artifact directory (optional - will use default if not set)
 export TEST_ARTIFACT_DIR=/tmp/cl_server_test_artifacts
+export UV_CACHE_DIR="${HOME}/.data/.uv_cache"
 
 # Default values
 SERVER_IP="localhost"
